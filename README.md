9b23b20837e45bdeb4033f60a0b4de7a1282b758
Rushil Patel <rushilp2311@gmail.com>
2020-10-03 05:39:34 -0400

    Authentication Added

---
+++ README.md
@@ -0,0 +1,1 @@
+# bug-trackr-backend
---
+++ models/user.js
@@ -0,0 +1,44 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(1).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
